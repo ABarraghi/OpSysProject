298a5b6aa3a249c5872c595aafab46664b45cfe4
abarraghi <abarraghi@gmail.com>
2025-02-14 16:53:16 -0500

    Make logic for node

--- src/include/node.py
+++ src/include/node.py
@@ -2,3 +2,7 @@
 #Defining a Node object for use in the LinkedList
 #First created Feb 14 2025
 #Last modified Feb 14 2025
+class Node: 
+    def __init__(self,data):
+        self.data = data
+        self.next = None
