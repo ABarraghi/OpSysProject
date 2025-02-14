c1a712abf28366d4126497643582ff61b9a77b96
unknown <shinycup51@gmail.com>
2025-02-13 20:25:52 -0500

    add main.py

--- src/input_file_creator.py
+++ src/input_file_creator.py
@@ -3,9 +3,7 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-job_list = [{"job_number": 1, "time_to_complete": 100, "time_completed": 0},
-             {"job_number": 2, "time_to_complete": 300, "time_completed": 0},
-             {"job_number": 3, "time_to_complete": 400, "time_completed": 0}]
+job_list = [{"job_number": 1, "time_to_complete": 100, "time_completed": 0}]
 
 with open("jobs.json", "w") as file:
-    json.dump(job_list, file, indent=4)
+    json.dump(job_list, file, indent=1)
---
+++ src/main.py
@@ -0,0 +1,17 @@
+import argparse
+
+# Create an argparse object
+parser = argparse.ArgumentParser(description="Job scheduler simulator")
+parser.add_argument("-I", "--input", type=str, help="Use a specific json job file, default is jobs.json")
+parser.add_argument("-R", "--roundrobin", action="store_true", help="Use the round robin job scheduler")
+
+# parse args
+args = parser.parse_args()
+
+file_input = "jobs.json"
+
+# Define file input
+if args.input:
+    file_input = args.input
+if args.roundrobin:
+    print("Round Robin enabled")
